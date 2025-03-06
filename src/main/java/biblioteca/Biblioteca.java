f71c0ea81751d6668cdae3898853d0dfdab98372
Luis Calzadilla <luis.calzadilla198@comunidadunir.net>
2025-03-06 13:49:05 +0100

    UPDATE: comentarios agregados para javadoc en Biblioteca.java y Libro.java

--- src/main/java/biblioteca/Biblioteca.java
+++ src/main/java/biblioteca/Biblioteca.java
@@ -3,24 +3,45 @@ package biblioteca;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Representa la estructura de una biblioteca
+ * Pertime agregar, eliminar y encontrar libros
+ * @see Libro
+ * @author Luis Calzadilla Romero
+ * @version 6.3.2025
+ */
+
 public class Biblioteca {
 
     // TODO: Documentar estos atributos
+    /** Lista de libros disponibles en la biblioteca */
     private final List<Libro> libros;
 
     // TODO: Documentar este método
+    /** Contructor por defecto de la biblioteca sin libros */
     public Biblioteca() {
         libros = new ArrayList<>();
     }
 
     // TODO: Documentar este método.
     //  Test: NO HAY QUE TESTEAR ESTE METODO
+
+    /**
+     *Contructor con parametros
+     * @param libros lista de libros
+     */
     public Biblioteca(List<Libro> libros) {
         this.libros = libros;
     }
 
     // TODO: Testear este metodo.
     //  Test: Comprobar si se ha agregado
+
+    /**
+     * Agrega un libro a la biblioteca
+     * @param libro un libro para agregar
+     * @return {@code true} si el libro se agrega o {@code false} si no se agrega
+     */
     public boolean agregarLibro(Libro libro) {
         return libros.add(libro);
     }
@@ -32,6 +53,11 @@ public class Biblioteca {
     }
 
     // TODO: Documentar este método
+
+    /**
+     * Devuelve la lista de libros de la biblioteca
+     * @return  devuelve todos los libros
+     */
     public List<Libro> getLibros() {
         return libros;
     }
@@ -39,6 +65,12 @@ public class Biblioteca {
     // TODO: Documentar este método.
     //  Test 01: buscar libro existente y comprobar que lo localiza.
     //  Test 02: buscar libro NO existente y comprobar que no lo localiza.
+
+    /**
+     * Devuelve un libro con un titulo especifico
+     * @param titulo titulo del libro a buscar
+     * @return libro con el titulo buscado
+     */
     public Libro encuentraLibroPorTitulo(String titulo) {
         for (Libro libro : libros) {
             if (libro.getTitulo().equals(titulo)) {
@@ -54,6 +86,14 @@ public class Biblioteca {
     //  Recuerda: las anotaciones @deprecated y @link a la nueva
     //  versión mejorada encuentraLibrosPorAutor(...)
     //  En esta ocasión, NO TESTEAREMOS este metodo
+
+    /**
+     * busca un libro de un autor determinado
+     * @deprecated metodo obsoleto
+     * se recomienda usae {@link #encuentraLibrosPorAutor(String)} en su lugar
+     * @param autor nombre del autor de un libro
+     * @return libro del autor si lo encuentra o null si no encuentra algun libro
+     */
     public Libro encuentaLibroPorAutor(String autor) {
         for (Libro libro : libros) {
             if (libro.getAutor().equals(autor)) {
@@ -69,6 +109,14 @@ public class Biblioteca {
     // TODO: Testear este metodo.
     //  Test: Comprobar la lista de libros que devuelve para un autor existentes.
     //  Test: Comprobar la lista de libros que devuelve para un autor No existente
+
+    /**
+     * busca libros de un autor determinado
+     * @since V2.0
+     * sustituye al metodo {@link #encuentraLibroPorTitulo(String)}
+     * @param autor nombre del autor de uno o varios libros
+     * @return lista de libros del autor
+     */
     public List<Libro> encuentraLibrosPorAutor(String autor) {
         List<Libro> listaLibros = null;
         for (Libro libro : libros) {
--- src/main/java/biblioteca/Libro.java
+++ src/main/java/biblioteca/Libro.java
@@ -1,8 +1,20 @@
 package biblioteca;
 
+/**
+ * Representa la estructura de  un libro
+ * Pertime crear libros
+ *
+ * @author Luis Calzadilla Romero
+ * @version 6.3.2025
+ */
+
 public class Libro {
 
     // TODO: Documentar estos atributos
+    /**
+     * titulo, autor y anioPublicacion : Son atributos de la clase para los objetos tipo libro
+     */
+
     private String titulo;
     private String autor;
     private int anioPublicacion;
@@ -10,6 +22,13 @@ public class Libro {
 
     // TODO: Documentar este método
     // TODO: Testear este método
+
+    /**
+     * Constructor de la clase para objetos tipo libro, recibe tres parametros
+     * @param titulo es el titulo que tendra el libro
+     * @param autor nombre del autor del libro
+     * @param anioPublicacion fecha en la que se publica el libro
+     */
     public Libro(String titulo, String autor, int anioPublicacion) {
         this.titulo = titulo;
         this.autor = autor;
@@ -25,6 +44,11 @@ public class Libro {
     }
 
     // TODO: Documentar este método
+
+    /**
+     * devuelve el nombre de autor del libro
+     * @return devuelve autor
+     */
     public String getAutor() {
         return autor;
     }
@@ -34,6 +58,11 @@ public class Libro {
     }
 
     // TODO: Documentar este método
+
+    /**
+     * devuelve la fecha de publicacion
+     * @return fecha de publicacion
+     */
     public int getAnioPublicacion() {
         return anioPublicacion;
     }
